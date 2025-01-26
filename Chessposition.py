dfcbe1e11ef5582741c3b70979eab4eab31b8249
Julian <julian.marx@hotmail.de>
2025-01-26 20:08:35 +0100

    Added functions determining legal moves and a function playing random games. There still seem to be errors in the determination of the legal moves.
    Still missing: Queening

--- Chessposition.py
+++ Chessposition.py
@@ -1,5 +1,9 @@
 from enum import Enum
 from tabulate import tabulate
+from LegalPieceMoves import *
+import random
+from time import sleep
+from copy import deepcopy
 
 class ChessPieces(Enum):
     WhitePawn = 1
@@ -18,8 +22,8 @@ class ChessPieces(Enum):
     def is_white(self):
         return self.value <= 6
 
-    def is_black(self):
-        return self.value > 6
+    def is_pawn(self):
+        return self==ChessPieces.WhitePawn or self==ChessPieces.BlackPawn
     
     def symbol(self) -> str:
         match self:
@@ -50,13 +54,10 @@ class ChessPieces(Enum):
     
     def invertcolor(self):
         return ChessPieces(self.value-6 if self.value>6 else self.value+6)
-    
-    def associateMoves(self):
-        pass
         
 
 class ChessPosition():
-    def __init__(self, Board: list[list[ChessPieces|None]]|None=None):
+    def __init__(self, Board: list[list[ChessPieces|None]]|None=None,whitesmove: bool= True,enpassantablefile: int|None=None):
         if Board==None:
             Board=[[None]*8 for Row in range(8)]
             Board[0]=[ChessPieces.WhiteRook,ChessPieces.WhiteKnight,ChessPieces.WhiteBishop,ChessPieces.WhiteQueen,ChessPieces.WhiteKing,ChessPieces.WhiteBishop,ChessPieces.WhiteKnight,ChessPieces.WhiteRook]
@@ -64,8 +65,118 @@ class ChessPosition():
             Board[1]=[ChessPieces.WhitePawn]*8
             Board[-2]=[ChessPieces.BlackPawn]*8
         self.Board=Board
+        self.whitesmove=whitesmove
+        self.enpassantablefile=enpassantablefile
+        #print(f"Initalizing: {str(self)}")
     def __str__(self):
-        return tabulate([["" if Piece is None else Piece.symbol() for Piece in row] for row in self.Board],tablefmt="grid")
+        return tabulate([["" if Piece is None else Piece.symbol() for Piece in row] for row in self.Board[::-1]],tablefmt="grid")
+    
+    def possibleMoves(self) -> list["ChessPosition"]:
+        ReachablePositions=[]
+        for rownumber in range(8):
+            for columnnumber in range(8):
+                piece_on_current_square=self.Board[rownumber][columnnumber]
+                #print(piece_on_current_square)
+                if piece_on_current_square!=None:
+                    piecesmoves=self.piecessemilegalmoves(rownumber,columnnumber)
+                    resulting_positions=[self.applymove(rownumber,columnnumber,*move) for move in piecesmoves]
+                    ReachablePositions+=[position for position in resulting_positions if not position.nonmovingPlayerinCheck()]
+        return ReachablePositions
+    
+    def randommove(self)-> "ChessPosition":
+        return random.choice(self.possibleMoves())
+
+
+    def findnonMovingPlayersKing(self):
+        searchedKing=ChessPieces.WhiteKing if not self.whitesmove else ChessPieces.BlackKing
+        for rownumber in range(8):
+            for columnnumber in range(8):
+                if self.Board[rownumber][columnnumber]==searchedKing:
+                    return rownumber,columnnumber
+                
+    def nonmovingPlayerinCheck(self):
+        kingssquare: tuple[int,int]=self.findnonMovingPlayersKing()
+        return kingssquare in self.reachablesquares()
+
+    def reachablesquares(self) -> list[tuple[int,int]]:
+        """Returns: All possible moves a piece any piece can make. Move might be illegal if own king hangs afterwards.
+        
+        Check, if pawn queened or enpassant happened before applying."""
+        ReachableSquares=[]
+        for rownumber in range(8):
+            for columnnumber in range(8):
+                piece_on_current_square=self.Board[rownumber][columnnumber]
+                if piece_on_current_square is not None:
+                    CurrentPiecesMoves=self.piecessemilegalmoves(rownumber,columnnumber)
+                    ReachableSquares+=[newsquare[:2] for newsquare in CurrentPiecesMoves]
+        return ReachableSquares
+        
+
+
+    def piecessemilegalmoves(self, rowNumber: int, columnNumber: int)-> list:
+        """Returns: All possible moves a piece on a certain square can make. Move might be illegal if own king hangs afterwards.
+        
+        Check, if pawn queened or enpassant happened before applying."""
+        piece=self.Board[rowNumber][columnNumber]
+        Rawmoves:list[tuple[int,int]|tuple[int,int,bool]]=[]
+        match piece:
+            case None:
+                raise ValueError(f"There is no piece on {(rowNumber,columnNumber)}")
+            case ChessPieces.BlackPawn:
+                Rawmoves=legalPawnmoves(rowNumber,columnNumber,False,self.Board,self.enpassantablefile)
+            case ChessPieces.WhitePawn:
+                Rawmoves=legalPawnmoves(rowNumber,columnNumber,True,self.Board,self.enpassantablefile)
+            case ChessPieces.BlackKnight|ChessPieces.WhiteKnight:
+                Rawmoves=legalknightmoves(rowNumber,columnNumber,self.Board)
+            case ChessPieces.WhiteBishop|ChessPieces.BlackBishop|ChessPieces.BlackQueen|ChessPieces.WhiteQueen:
+                Rawmoves+=(legaldiagonalmoves(rowNumber,columnNumber,self.Board))
+            case ChessPieces.WhiteRook|ChessPieces.BlackRook|ChessPieces.BlackQueen|ChessPieces.WhiteQueen:
+                Rawmoves+=(legalstraightmoves(rowNumber,columnNumber,self.Board))
+            case ChessPieces.WhiteKing | ChessPieces.BlackKing:
+                Rawmoves+=legalkingmoves(rowNumber,columnNumber,self.Board)
+            case _:
+                ValueError(f"Did not expect {piece =}")
+        Actualmoves=[]
+        for move in Rawmoves:
+            newrow=move[0]
+            newcol=move[1]
+            Beatenpiece=self.Board[newrow][newcol]
+            if Beatenpiece is None:
+                Actualmoves.append((newrow,newcol))
+            elif Beatenpiece.is_white()!=piece.is_white():
+                Actualmoves.append((newrow,newcol))
+        """print(str(self))
+        print(f"Legal moves of piece on {(rowNumber,columnNumber)} = {Actualmoves}")"""
+        return Actualmoves
+    def applymove(self, startrow: int, startcolumn: int, endrow: int, endcolumn: int, enpassanthappened: bool = False):
+        """Copyself=deepcopy(self)
+        print(str(self))
+        print(f"{startrow = }, {startcolumn = }, {endrow = }, {endcolumn = }, {self.Board[startrow][startcolumn] = }")"""
+        NewBoard=deepcopy(self.Board)
+        if enpassanthappened:
+            enpassantrow= 3 if self.whitesmove else 4
+            NewBoard[enpassantrow][self.enpassantablefile]=None
+        movedpiece=self.Board[startrow][startcolumn]
+        NewBoard[endrow][endcolumn]=movedpiece
+        NewBoard[startrow][startcolumn]=None
+        newenpassantablefile:int|None=None
+        if movedpiece.is_pawn() and abs(startrow-endrow)==2:
+            newenpassantablefile=startcolumn
+        return ChessPosition(NewBoard,not self.whitesmove,newenpassantablefile)
+
+        
+
+        
+def randomgame():
+    Position=ChessPosition()
+    while True:
+        print(str(Position))
+        Position=Position.randommove()
+        sleep(3)
+
+
+
     
 if __name__=="__main__":
-    print(str(ChessPosition()))
+    ChessPosition().randommove()
+    pass
---
+++ LegalPieceMoves.py
@@ -0,0 +1,105 @@
+def legalPawnmoves(row: int, column: int, WhitePawn:bool, Board: list[list],enpassantablefile: int|None)-> list[tuple[int,int,bool]]:
+    """Returns: list of legal moves containing row, column and wether enpassant happened.
+     
+     List of legal Moves includes the moves which are physically possible. The moves might leave the king hanging or capture own pieces.
+    """
+    if not WhitePawn:
+        MirroredMoves: list[tuple[int,int,bool]]=legalPawnmoves(7-row,column,True, Board, enpassantablefile)
+        return [(7-Row, Column,enpassanthappened) for (Row,Column,enpassanthappened) in MirroredMoves]
+    
+    Moves=[]
+    #Straightmoves
+    if Board[row+1][column] is None:
+        Moves.append((row+1,column,False))
+        if row==1:
+            if Board[row+2][column]:
+                Moves.append((row+2,column,False))
+    #En passant
+    if enpassantablefile is not None and row==4:
+        if abs(enpassantablefile-column)==1:
+            Moves.append((5,enpassantablefile,True))
+    #Normal captures
+    if column+1<8:
+        if Board[row+1][column+1] is not None:
+            Moves.append((row+1,column+1,False))
+    if column-1>=0:
+        if Board[row+1][column-1] is not None:
+            Moves.append((row+1,column-1,False))
+    return Moves
+
+def legalstraightmoves(row: int, column: int, Board: list[list])-> list[tuple[int,int]]:
+    """Returns: list of legal moves containing row and column.
+     
+     List of legal Moves includes the moves which are physically possible. The moves might leave the king hanging or capture own pieces.
+    """
+    #Upward moves
+    Moves=movesincertaindirection(row,column,Board,1,0)
+    #Downward moves
+    Moves+=movesincertaindirection(row,column,Board,-1,0)
+    #Rightward moves
+    Moves+=movesincertaindirection(row,column,Board,0,1)
+    #Leftward moves
+    Moves+=movesincertaindirection(row,column,Board,0,-1)
+    return Moves
+    """Moves=[]
+    #Upward moves
+    if Row!=7:
+        newrow=Row+1
+        Moves.append((newrow, Column))
+        while newrow+1<8 and Board[newrow, Column] is None:
+            Moves.append((newrow+1, Column))
+            newrow+=1
+    #Downward moves
+    if Row!=0:
+        newrow=Row-1
+        Moves.append((newrow, Column))
+        while newrow-1>=0 and Board[newrow, Column] is None:
+            Moves.append((newrow-1, Column))
+            newrow-=1
+    
+    #Leftward moves
+    if Column!=0:
+        newcol=Column-1
+        Moves.append((Row, newcol))
+        while newcol-1>=0 and Board[Row, newcol] is None:
+            Moves.append((Row, newcol-1))
+            newcol-=1
+    #Upward moves
+    if Column!=7:
+        newcol=Column+1
+        Moves.append((Row, newcol))
+        while newcol+1>=0 and Board[Row, newcol] is None:
+            Moves.append((Row, newcol+1))
+            newcol+=1
+        return Moves"""
+def movesincertaindirection(row: int, column: int, Board: list[list],rowincrement: int,columnincrement: int) -> list[tuple[int,int]]:
+        Moves=[]
+        newrow=row
+        newcol=column
+        while 0<=newrow+rowincrement<8 and 0<=newcol+columnincrement<8 and ((Board[newrow][newcol] is None) or (row,column)==(newrow,newcol)):
+            newcol+=columnincrement
+            newrow+=rowincrement
+            Moves.append((newrow,newcol))
+        return Moves
+
+def legaldiagonalmoves(row: int, column: int, Board: list[list])-> list[tuple[int,int]]:
+    """Returns: list of legal moves containing row and column.
+     
+     List of legal Moves includes the moves which are physically possible. The moves might leave the king hanging or capture own pieces.
+    """
+    Moves=movesincertaindirection(row,column,Board,1,1)
+    Moves+=movesincertaindirection(row,column,Board,-1,1)
+    Moves+=movesincertaindirection(row,column,Board,1,-1)
+    Moves+=movesincertaindirection(row,column,Board,-1,-1)
+    return Moves
+
+def legalknightmoves(row: int, column: int, Board: list[list])-> list[tuple[int,int]]:
+    Vectors=[(2,1),(1,2)]
+    Factors=[-1,1]
+    theoreticalmoves=[(factorrow*i,factorcol*j) for i,j in Vectors for factorcol in Factors for factorrow in Factors]
+    return [(row+i,column+j) for (i,j) in theoreticalmoves if (0<=row+i<8 and 0<=column+j<8)]
+
+def legalkingmoves(row: int, column: int, Board: list[list])-> list[tuple[int,int]]:
+    ammounts=[-1,0,1]
+    theoreticalmoves=[(leftstep,rightstep) for leftstep in ammounts for rightstep in ammounts if (leftstep,rightstep)!=(0,0)]
+    return [(row+i,column+j) for (i,j) in theoreticalmoves if (0<=row+i<8 and 0<=column+j<8)]
